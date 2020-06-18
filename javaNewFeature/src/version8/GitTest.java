d16e5cdfe9b32ffd1ef0ac420c17f06f0bdf4581
zlk <1950475445@qq.com>
2020-06-18 12:16:52 +0800

    idea github推送测试

---
+++ javaNewFeature/src/version8/GitTest.java
@@ -0,0 +1,13 @@
+package version8;
+
+/**
+ * @description:
+ * @author: slfang
+ * @time: 2020/6/18 12:15
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("gitPushTest");
+    }
+}
